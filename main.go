428ef83cbb608e658dfb261ec0a62308944f8bd2
Dmitriy Ponomar <ppoonncchhiikk@gmail.com>
2021-01-02 13:29:54 +0300

    Add(go-the-complete-developers-guide): Custom Type Declarations

--- main.go
+++ main.go
@@ -1,22 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	// Array has fixed size. In this example it's 3.
-	// cards := [3]string{"Ace of Diamonds", newCard()}
-	// cards[2] = "Six of Spades"
-	// for i := 0; i < len(cards); i++ {
-	// 	fmt.Println(i, cards[i])
-	// }
-
-	// Slice could grow or shrink
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
