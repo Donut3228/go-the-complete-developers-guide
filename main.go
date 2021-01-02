fcf5ed110a219dd482e178ad0e7b78030606bf0f
Dmitriy Ponomar <ppoonncchhiikk@gmail.com>
2021-01-02 14:59:27 +0300

    Add(go-the-complete-developers-guide): Saving deck to the disk

--- main.go
+++ main.go
@@ -2,13 +2,5 @@ package main
 
 func main() {
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 4)
-
-	hand.print()
-	remainingCards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards.saveToFile("my_cards")
 }
