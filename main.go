f4228c41568c2dc2cd1efdba58b49f3f02d01f04
Dmitriy Ponomar <ppoonncchhiikk@gmail.com>
2021-01-02 13:00:51 +0300

    Add(go-the-complete-developers-guide): Slices and For Loops

--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
+	// Array has fixed size. In this example it's 3.
+	// cards := [3]string{"Ace of Diamonds", newCard()}
+	// cards[2] = "Six of Spades"
+	// for i := 0; i < len(cards); i++ {
+	// 	fmt.Println(i, cards[i])
+	// }
 
-	fmt.Println(card)
+	// Slice could grow or shrink
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
