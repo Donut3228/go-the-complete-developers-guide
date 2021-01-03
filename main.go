39b83b42377cc96f5336a1a7913a51e98658f79c
Dmitriy Ponomar <ppoonncchhiikk@gmail.com>
2021-01-04 00:28:24 +0300

    Add(go-the-complete-developers-guide): Examples of struct and receiver functions

--- main.go
+++ main.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
 func main() {
-	fmt.Println("Template")
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Halpert",
+		contactInfo: contactInfo{
+			email:   "jim.halpert@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jim.updateName("Alex")
+	jim.print()
+}
+
+func (p *person) updateName(newName string) {
+	p.firstName = newName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
