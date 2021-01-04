50ba9c2f2db9560c420301c9f6232af7ac0b122f
Dmitriy Ponomar <ppoonncchhiikk@gmail.com>
2021-01-04 10:11:24 +0300

    Add(go-the-complete-developers-guide): Examples of map initialization

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	// var colors map[string]string
+
+	// colors := make(map[string]string)
+	// colors["red"] = "#ff0000"
+	// colors["green"] = "#00ff00"
+
 	colors := map[string]string{
 		"black": "#000000",
 		"white": "#ffffff",
