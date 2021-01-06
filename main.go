24078eee1174bf81f640b21ce2b1d2a7c84ebc41
Dmitriy Ponomar <ppoonncchhiikk@gmail.com>
2021-01-06 06:37:56 +0300

    Add(go-the-complete-developers-guide): PrintPerimeter function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type shape interface {
 	getArea() float64
+	getPerimeter() float64
 }
 
 type triangle struct {
@@ -17,18 +18,30 @@ type square struct {
 func (t triangle) getArea() float64 {
 	return 0.5 * t.base * t.height
 }
+func (t triangle) getPerimeter() float64 {
+	return 2*t.height + t.base
+}
 
 func (s square) getArea() float64 {
 	return s.sideLength * s.sideLength
+}
+func (s square) getPerimeter() float64 {
+	return 4 * s.sideLength
 }
 
 func printArea(sh shape) {
 	fmt.Println(sh.getArea())
 }
 
+func printPerimeter(sh shape) {
+	fmt.Println(sh.getPerimeter())
+}
+
 func main() {
 	t := triangle{base: 10, height: 15}
 	s := square{sideLength: 35}
 	printArea(t)
+	printPerimeter(t)
 	printArea(s)
+	printPerimeter(s)
 }
