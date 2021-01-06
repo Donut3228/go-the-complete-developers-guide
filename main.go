133888ad21c702474842b4ac4e13fa759392e888
Dmitriy Ponomar <ppoonncchhiikk@gmail.com>
2021-01-06 06:57:07 +0300

    Add(go-the-complete-developers-guide): Assignment solution

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("Template")
+	args := os.Args
+	if len(args) <= 1 {
+		fmt.Println("Expected to have a file name as a first argument")
+		os.Exit(1)
+	}
+
+	fileName := os.Args[1]
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, file)
 }
